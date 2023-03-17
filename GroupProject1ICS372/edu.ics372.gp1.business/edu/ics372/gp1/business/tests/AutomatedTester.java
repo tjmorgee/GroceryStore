fafe97535f4bea5d10fa369d6cbe4f3b795f2dba
Jonathon Voss <jonathon.voss@my.metrostate.edu>
2023-03-17 13:45:05 -0500

    More tests

--- GroupProject1ICS372/edu.ics372.gp1.business/edu/ics372/gp1/business/tests/AutomatedTester.java
+++ GroupProject1ICS372/edu.ics372.gp1.business/edu/ics372/gp1/business/tests/AutomatedTester.java
@@ -4,16 +4,19 @@ import edu.ics372.gp1.business.facade.Request;
 import edu.ics372.gp1.business.facade.Result;
 
 import java.util.GregorianCalendar;
+import java.util.Iterator;
 public class AutomatedTester {
-	int FullWordsBecauseIAmNotSupposedToUseTheLetterI = 0;
-	private static final int TESTS = 3;
+	private static final int TESTS = 4;
 	private GroceryStore groceryStore = GroceryStore.instance();
-	private String[] names = {"name1", "name2", "name3"};
-	private String[] addresses = {"address1", "address2", "address3"};
-	private String[] phones = {"phone1", "phone2", "phone3"};
-	private double[] fee = {599.22, 48, 483.2};
-	private String[] dates = {"d1", "d2", "d3"};
+	private String[] names = {"name1", "name2", "name3", "name1"};
+	private String[] addresses = {"address1", "address2", "address3", "address4"};
+	private String[] phones = {"phone1", "phone2", "phone3", "phone4"};
+	private double[] fee = {599.22, 48, 483.2, 4};
+	private String[] dates = {"d1", "d2", "d3", "d4"};
 //	private GregorianCalendar[] dates = new GregorianCalendar[3];
+	
+	private double[] prices = {1.0, 20.0, 33.3, 44.4};
+	private int[] reorderLevels = {10, 20, 30, 40};
 
 	public AutomatedTester() {
 		test();
@@ -35,7 +38,53 @@ public class AutomatedTester {
 		}
 	}
 	
+	private void testRemoveMember() {
+		Request.instance().reset();
+		Request.instance().setMemberName(names[1]);
+		Result result = groceryStore.searchMembership(Request.instance());
+		assert result.getResultCode() == Result.OPERATION_COMPLETED;
+		Request.instance().setMemberId(result.getMemberId());
+		result = groceryStore.removeMember(Request.instance());
+		assert result.getResultCode() == Result.OPERATION_COMPLETED;
+		result = groceryStore.searchMembership(Request.instance());
+		assert result.getResultCode() == Result.NO_SUCH_MEMBER;
+	}
+	
+	private void testRetrieveMember() {
+		Result result;
+		Request.instance().setMemberName(names[0]);
+		result = groceryStore.searchMembership(Request.instance());
+		assert result.getResultCode() == Result.OPERATION_COMPLETED;
+		assert result.getMemberName() == names[0];
+		assert result.getMemberAddress() == addresses[0];
+		assert result.getMemberPhone() == phones[0];
+		assert result.getMemberFee() == fee[0];
+		assert result.getMemberId().equals("M1");
+	
+		result.setResultCode(-1);
+		Iterator<Result> iterator = groceryStore.getMembers();
+		iterator.next();
+		while (iterator.hasNext()) {
+			Result temp = iterator.next();
+			if (result.getMemberName().equalsIgnoreCase(temp.getMemberName())) {
+				assert temp.getMemberName() == names[3];
+				assert temp.getMemberAddress()  == addresses[3];
+				assert temp.getMemberPhone()  == phones[3];
+				assert temp.getMemberFee() == fee[3];
+				assert temp.getMemberId().equals("M4");
+				result.setResultCode(Result.OPERATION_COMPLETED);
+			}
+		}
+			assert result.getResultCode() == Result.OPERATION_COMPLETED; 
+	}
+	
+	private void testAddProducts() {
+		Result result;
+	}
+	
 	public void test() {
 		testAddMember();
+		testRemoveMember();
+		testRetrieveMember();
 	}
 }
